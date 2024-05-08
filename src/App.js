a99a43fd55a8977832a002be6b133cb6cdba600f
Md. Nurul Haider Hridoy <hridoy0182726@gmail.com>
2024-05-08 13:47:22 +0600

    lec-15(Get input box  value)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+// lec-15 Get Input box value
 import './App.css';
+import React,{useState} from 'react';
 
 function App() {
+const [data,setData]=useState(null) 
+const [print,setPrint]=useState(false) 
+
+  function getData(val)
+  {
+    console.warn(val.target.value)
+    setData(val.target.value)
+    setPrint (false)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        print?
+        <h1> {data} </h1>
+        :null
+      }
+       
+      <input type ="text" onChange={getData}/>
+      <button onClick={()=>setPrint(true)}> Print Data </button>
+      
     </div>
   );
 }
