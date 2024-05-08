2041cfe2aebba37a5473049f4bda6f2605d77316
Md. Nurul Haider Hridoy <hridoy0182726@gmail.com>
2024-05-08 15:27:10 +0600

    Lec-16 (Hide,Show and Toggle).

--- src/App.js
+++ src/App.js
@@ -1,30 +1,61 @@
-// lec-15 Get Input box value
-import './App.css';
-import React,{useState} from 'react';
+// // lec-15 Get Input box value
+// import './App.css';
+// import React,{useState} from 'react';
 
-function App() {
-const [data,setData]=useState(null) 
-const [print,setPrint]=useState(false) 
+// function App() {
+// const [data,setData]=useState(null) 
+// const [print,setPrint]=useState(false) 
 
-  function getData(val)
-  {
-    console.warn(val.target.value)
-    setData(val.target.value)
-    setPrint (false)
-  }
-  return (
-    <div className="App">
-      {
-        print?
-        <h1> {data} </h1>
-        :null
-      }
+//   function getData(val)
+//   {
+//     console.warn(val.target.value)
+//     setData(val.target.value)
+//     setPrint (false)
+//   }
+//   return (
+//     <div className="App">
+//       {
+//         print?
+//         <h1> {data} </h1>
+//         :null
+//       }
        
-      <input type ="text" onChange={getData}/>
-      <button onClick={()=>setPrint(true)}> Print Data </button>
+//       <input type ="text" onChange={getData}/>
+//       <button onClick={()=>setPrint(true)}> Print Data </button>
       
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// Lec-16  Start here ( Hide, Show and Toggle ).
+
+import React,{useState} from "react";
+
+function App()
+{
+  const [status,setStatus]=useState(false)
+  return (
+    <div className="app">
+
+      {
+        status? 
+        <h1> Hridoy Khan </h1>:null
+      }
+        
+        <button onClick={()=>setStatus(false)}> Hide </button>
+            <button onClick={()=>setStatus(true)}> Show </button>
+
+        {/* <button onClick={()=>setStatus(!status)}> Toggle </button> */}
+        
     </div>
+
   );
 }
 
 export default App;
+
+// Lec-16  Close here ( Hide, Show and Toggle ).
