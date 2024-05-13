d7adcbf450b07f1f278d7b579ec868ed235c4f1d
Md. Nurul Haider Hridoy <hridoy0182726@gmail.com>
2024-05-13 13:04:32 +0600

    Lec-43 ForwardRef in React

--- src/App.js
+++ src/App.js
@@ -437,37 +437,37 @@
 // // Lec-41 start Ref in React js using class component:
 
 
-import './App.css';
-import React,{createRef} from 'react'
-class App extends React.Component {
-  constructor()
-  {
-    super();
-    this.inputRef=createRef();
-  }
-  componentDidMount()
-  {
-    // console.warn(this.inputRef.current.value="1000")
-  }
-  getVal()
-  {
-    console.warn(this.inputRef.current.value)
-    this.inputRef.current.style.color="red"
-    this.inputRef.current.style.backgroundColor="black"
+// import './App.css';
+// import React,{createRef} from 'react'
+// class App extends React.Component {
+//   constructor()
+//   {
+//     super();
+//     this.inputRef=createRef();
+//   }
+//   componentDidMount()
+//   {
+//     // console.warn(this.inputRef.current.value="1000")
+//   }
+//   getVal()
+//   {
+//     console.warn(this.inputRef.current.value)
+//     this.inputRef.current.style.color="red"
+//     this.inputRef.current.style.backgroundColor="black"
 
-  }
-  render() {
-    return (
-      <div className="App">
-        <h1>Ref in React </h1>
-        <input type="text" ref={this.inputRef}  />
-        <button onClick={()=>this.getVal()}>Check Ref</button>
-      </div>
-    );
-  }
-}
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Ref in React </h1>
+//         <input type="text" ref={this.inputRef}  />
+//         <button onClick={()=>this.getVal()}>Check Ref</button>
+//       </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
 
 // Lec-41 end Ref in React js using class component:
 
@@ -498,6 +498,36 @@ export default App;
 
 
 // // Lec-42 end UseRef in React 
+
+// // Lec-43 Start forwardRef in Reactjs
+
+
+import './App.css';
+import React ,{useRef} from 'react'
+import User from './lec-43-forwardRef';
+function App() {
+  let inputRef=useRef(null)
+  function updateInput()
+  {
+    inputRef.current.value="1000";
+    inputRef.current.style.color="red"
+    inputRef.current.focus()
+
+
+  }
+  return (
+    <div className="App">
+      <h1>forwardRef in React </h1>
+      <User ref={inputRef} />
+      <button onClick={updateInput} >Update Input Box</button>
+    </div>
+  );
+
+}
+export default App;
+
+// // Lec-43 End forwardRef in Reactjs
+
 
 
 
---
+++ src/lec-43-forwardRef.js
@@ -0,0 +1,12 @@
+// // Lec-43 Start forwardRef in Reactjs
+import React,{forwardRef} from 'react'
+function User(props,ref)
+{
+    return(
+        <div>
+            <input ref={ref} type="text" />
+        </div>
+    )
+}
+
+export default forwardRef(User);
